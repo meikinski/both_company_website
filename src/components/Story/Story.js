178cc3263e75e0b3cc9c6e3c97ae8d60dfefeebd
Meike <meikehuettl@web.de>
2022-04-23 17:35:35 +0200

    nutzen home respon

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -14,7 +14,7 @@ export const Story = ({ imageSrc, title, text }) => {
             <h2 className='story_section_content_title'>{title}</h2>
             <p>Berufseinstieg, Aufstieg, Erfolge, Rollen im Unternehmen, Teamkonstellationen, Jobsuche, Veränderung, Mitarbeiterentwicklung, Konfliktsituationen – wir sind beide seit einigen Jahren in der Berufswelt angekommen und haben schon immer gerne gemeinsam über die Arbeit reflektiert, Situationen besprochen und uns gegenseitig unterstützt.
             </p>
-            <NavLink to="/ueberuns"><button className='button_black'>Über uns</button></NavLink>
+            <NavLink to="/ueberuns"><button className='story_button_black'>Über uns</button></NavLink>
         </div>
     </div>
   </div>  
