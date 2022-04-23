b0d893656a893595c0343d7a2e7d1681d51856a9
Meike <meikehuettl@web.de>
2022-04-23 14:02:51 +0200

    Angebot Page Respon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Home from './pages/Home/Home';
 import ÜberUns from './pages/ÜberUns/ÜberUns';
 import Analysetool from './pages/Analysetool/Analysetool';
-import Angebot from './pages/Angebot/Angebot';
+import AngebotPage from './pages/Angebot/AngebotPage';
 import Logo from "./assets/Logo.png"
 import KontaktPage from './pages/KontaktPage/KontaktPage';
 import Danke from './pages/Danke/Danke';
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/Home' element={<Home />}></Route>
         <Route path='/ueberuns' element={<ÜberUns />}></Route>
         <Route path='/analysetool' element={<Analysetool />}></Route>
-        <Route path='/angebot' element={<Angebot />}></Route>
+        <Route path='/angebot' element={<AngebotPage />}></Route>
         <Route path='/kontakt' element={<KontaktPage />}></Route>
         <Route path='/danke' element={<Danke />}></Route>
         <Route path='/impressum' element={<Impressum />}></Route>
