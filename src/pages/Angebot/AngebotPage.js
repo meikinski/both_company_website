afc6a3bb96009d33f9ef914e6f7420255781cb96
Meike <meikehuettl@web.de>
2023-09-29 12:27:53 +0200

    angebot final

--- src/pages/Angebot/AngebotPage.js
+++ src/pages/Angebot/AngebotPage.js
@@ -125,6 +125,52 @@ Entscheidungsfindung.</li><br></br>
                 <li>&raquo; Die erarbeiteten Inhalte sind für euch direkt im Arbeitsalltag nutzbar.</li>
               </ul>
             </div>
+            <div className="workshop_section_info">
+              <div className="workshop_info_side_left">
+                <div className="workshop_info_card">
+                <div className="workshop_info_icon">
+                  <img src={Datum} alt="Datum"/>
+                </div>
+                <div className="workshop_info_text">
+                  
+                  <p>Auf Anfrage</p>
+                </div>
+                </div>
+                <div className="workshop_info_card">
+                  <div className="workshop_info_icon">
+                    <img src={Uhr} alt="Uhr"/>
+                  </div>
+                  <div className="workshop_info_text">
+                  <p>Auf Anfrage</p>
+                  </div>
+                </div>
+                <div className="workshop_info_card">
+                  <div className="workshop_info_icon">
+                    <img src={Kosten} alt="Kosten"/>
+                  </div>
+                  <div className="workshop_info_text"> 
+                  <p>Auf Anfrage</p>
+                  
+                  </div>
+                </div>
+              </div>
+              <div className="workshop_info_side_right">
+                <div className="workshop_info_card" id="icon_ort">
+                  <div className="workshop_info_icon" >
+                    <img src={Ort} alt="Ort"/>
+                  </div>
+                  <div className="workshop_info_text">
+                    <p><b>Die Etage</b></p>
+                    <p>Kurfürstenstr. 6</p>
+                    <p>50678 Köln</p>
+                    
+                  </div>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2515.2093641227507!2d6.958710715484669!3d50.91984397954286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf25943850cffd%3A0x772f7788f0580e57!2sDie%20Etage%20-%20Workshops%2C%20Produktion%2C%20Streaming!5e0!3m2!1sde!2sde!4v1650205390620!5m2!1sde!2sde" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="karte"></iframe>
+              </div>
+              <br></br>
+                <p>Interesse? Dann kontaktiere uns gerne über unser <NavLink to="/kontakt">Kontaktformular</NavLink>.</p>
+            </div>
           <div className="workshop_section">
             <h2>Workshop: "DiSG® &amp; ME"</h2>
               <p>In unserem Einsteigerworkshop <b>"DiSG® &amp; ME"</b> wollen wir folgende Fragen beantworten: Wie bin ich? Wie wirke ich? Wie handle ich? Und was bringt mir dieses Wissen im Arbeitsalltag? </p> 
